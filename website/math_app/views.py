284c40918ca36cab6028be5dba628760e029bcc6
moravechynek <moravechynek@seznam.cz>
2022-11-08 11:43:05 +0100

    calc

--- website/math_app/views.py
+++ website/math_app/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Priklad
 
@@ -7,3 +8,6 @@ def index(request):
     priklady = Priklad.objects.all()
     output = ', '.join([p.priklad for p in priklady])
     return HttpResponse(output)
+
+def calc(request):
+    return render(request, "calc.html")
